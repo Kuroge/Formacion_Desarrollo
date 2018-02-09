04fae86de9111c900b1395f56227e17c88315166
Adrián García Riera <i7610@softgal.com>
2018-02-09 08:21:14 +0100

    Add project files.

---
+++ Formacion_Desarrollo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Formacion_Desarrollo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
